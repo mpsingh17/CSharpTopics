a209ee4c5dd60b5c0342483faa6effaff9df7b83
mpsingh17 <mpsingh17@outlook.co.nz>
2020-05-02 15:52:06 +1200

    Exploring enums methods to get constants and values.

--- Enumeration/Program.cs
+++ Enumeration/Program.cs
@@ -2,15 +2,15 @@
 
 namespace Enumeration
 {
-    public enum DaysInWeek
+    public enum DaysInWeek : short
     {
-        Monday,
-        Tuesday,
-        Wednesday,
-        Thursday,
-        Friday,
-        Saturday,
-        Sunday
+        Monday = 1,
+        Tuesday = 2,
+        Wednesday = 3,
+        Thursday = 4,
+        Friday = 5,
+        Saturday = 6,
+        Sunday = 7
     }
 
     class Program
@@ -18,8 +18,39 @@ namespace Enumeration
         static void Main(string[] args)
         {
             var day = DaysInWeek.Monday;
+
+            var dayValue = (int) day;
+
+
+            int random = 10;
+
+            //Console.WriteLine("Day of week: {0}", day);
+            //Console.WriteLine("Day of week Value: {0}", dayValue);
+
+            //Console.WriteLine("Day of week: {0}", (DaysInWeek) dayValue);
             
-            Console.WriteLine("Day of week: {0}", (int) day);
+            //Console.WriteLine("Random day: {0}", (DaysInWeek) random);
+
+            var allDays = Enum.GetValues(typeof(DaysInWeek));
+
+            
+            foreach (var d in allDays)
+            {
+                Console.WriteLine((int)(DaysInWeek)d);
+                //Console.WriteLine(Enum.GetName(typeof(DaysInWeek), (DaysInWeek) d));
+                //Console.WriteLine(d);
+
+            }
+            Console.WriteLine("----------------------------------------");
+
+            foreach (var name in Enum.GetNames(typeof(DaysInWeek)))
+            {
+                //Console.WriteLine(name == DaysInWeek.Monday.ToString());
+                var result = Enum.Parse(typeof(DaysInWeek), name);
+                Console.WriteLine((int) (DaysInWeek)result);
+            }
+
+
         }
     }
 }
